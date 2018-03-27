e9b6a114f97eb9f5b5523f949ea77ca5b1b3d971
LAPTOP-Q0RI1272\romy- <romybijkerk1996@gmail.com>
2018-03-27 14:39:22 +0200

    Final commit of the day

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -117,13 +117,13 @@ if(!$user_id){
           <!-- User Account: style can be found in dropdown.less -->
            <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-              <img src="dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
+              <img src="<?php echo base_url(); ?>dist/img/user2-160x160.jpg" class="user-image" alt="User Image">
               <span class="hidden-xs"><?php echo $this->session->userdata('user_name'); ?></span>
             </a>
             <ul class="dropdown-menu">
             
               <li class="user-header">
-                <img src="dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <img src="<?php echo base_url(); ?>dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
 
                 <p>
                   <?php echo $this->session->userdata('user_name'); ?> - Web Developer
